4e660b9a9cbf39a6a3dfc3a49e45fe64dbd160c8
Karya Ülker <ulkerkarya@gmail.com>
2022-12-01 23:18:52 +0300

    hello

--- Main.py
+++ Main.py
@@ -1,1 +1,6 @@
-print('Hello')
+character_name= "John"
+character_age= "35"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really killed the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
